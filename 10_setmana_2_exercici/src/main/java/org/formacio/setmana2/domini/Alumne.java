cb8150a82a2d27c98272405925a7daaf78cad2f2
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-18 18:30:21 +0200

    test_mapping_alumne passed :D

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Alumne.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Alumne.java
@@ -1,22 +1,38 @@
 package org.formacio.setmana2.domini;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_alumnes")
 public class Alumne {
 
-	private String nom;
-	
-	private int edat;
-	
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public int getEdat() {
-		return edat;
-	}
-	public void setEdat(int edat) {
-		this.edat = edat;
-	}
-	
+    /* ---- Properties ---- */
+    @Id
+    @Column(name = "alu_nom")
+    private String nom = null;
+    @Column(name = "alu_edat")
+    private int edat = 0;
+
+
+    /* ---- Getters ---- */
+    public String getNom() {
+        return nom;
+    }
+
+    public int getEdat() {
+        return edat;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setEdat(int edat) {
+        this.edat = edat;
+    }
 }
