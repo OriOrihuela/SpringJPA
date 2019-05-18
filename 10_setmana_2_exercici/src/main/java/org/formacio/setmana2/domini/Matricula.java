3c6c7475ad2d377a981c1e2db1153b5b74bc64c4
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-18 18:57:31 +0200

    test_mapping_matricula passed :D

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Matricula.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Matricula.java
@@ -1,32 +1,48 @@
 package org.formacio.setmana2.domini;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "t_matricules")
 public class Matricula {
 
-	private Long id; 
-	
-	private Alumne alumne;
-	
-	private Curs curs;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Alumne getAlumne() {
-		return alumne;
-	}
-	public void setAlumne(Alumne alumne) {
-		this.alumne = alumne;
-	}
-	public Curs getCurs() {
-		return curs;
-	}
-	public void setCurs(Curs curs) {
-		this.curs = curs;
-	}
-	
-	
+    /* ---- Properties ---- */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mat_id")
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "mat_alumne")
+    private Alumne alumne;
+    @OneToOne
+    @JoinColumn(name = "mat_curs")
+    private Curs curs;
+
+
+    /* ---- Getters ---- */
+    public Long getId() {
+        return id;
+    }
+
+    public Alumne getAlumne() {
+        return alumne;
+    }
+
+    public Curs getCurs() {
+        return curs;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAlumne(Alumne alumne) {
+        this.alumne = alumne;
+    }
+
+    public void setCurs(Curs curs) {
+        this.curs = curs;
+    }
 }
