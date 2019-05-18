ae6958030b2cd01612b25f3ba47f2f28bf662721
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-18 19:36:07 +0200

    test_multiples_alumnes_ok passed :D

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/servei/ServeiEscola.java
@@ -1,21 +1,35 @@
 package org.formacio.setmana2.servei;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.formacio.setmana2.domini.Matricula;
 import org.formacio.setmana2.repositori.EdatIncorrecteException;
+import org.formacio.setmana2.repositori.RepositoriEscola;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ServeiEscola {
 
-	
-	/**
-	 * Important: els alumnes i els cursos indicats JA existeixen a la base de dades.
-	 * Per tant, els hem de carregar, no crear de nou.
-	 * L'excepcio EdatIncorrecteException no s'ha de capturar. S'ha de propagar cap el client
-	 */
-	public List<Matricula> apunta (String curs, List<String> alumnes) throws EdatIncorrecteException {
-		return null;
+    /* ---- "Properties" ---- */
+    @Autowired
+    private RepositoriEscola repositoriEscola = null;
+
+    /**
+     * Important: els alumnes i els cursos indicats JA existeixen a la base de dades.
+     * Per tant, els hem de carregar, no crear de nou.
+     * L'excepcio EdatIncorrecteException no s'ha de capturar. S'ha de propagar cap el client
+     */
+
+    /* ---- Behaviours ---- */
+    public List<Matricula> apunta(String curs, List<String> alumnes) throws EdatIncorrecteException {
+        List<Matricula> matriculas = new ArrayList<>();
+        for (String alumne : alumnes) {
+            matriculas.add(repositoriEscola.apunta(alumne, curs));
+        }
+        return matriculas;
     }
 }
