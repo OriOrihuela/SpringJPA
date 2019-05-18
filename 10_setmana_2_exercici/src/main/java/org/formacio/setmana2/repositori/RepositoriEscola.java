ee96b1e910c1f5881975acaebc5dada350119aa6
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-18 19:09:16 +0200

    test_carrega_curs passed :D

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -4,6 +4,9 @@ import org.formacio.setmana2.domini.Curs;
 import org.formacio.setmana2.domini.Matricula;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les
  * operacions de persistencia tal com indiquen les firmes dels metodes
@@ -11,9 +14,20 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class RepositoriEscola {
 
+    /* ---- Properties ---- */
+    @PersistenceContext
+    private EntityManager entityManager = null;
+
+
+    /* ---- Getters ---- */
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
     /* ---- Behaviours ---- */
     public Curs carregaCurs(String nom) {
-        return null;
+        Curs curs = getEntityManager().find(Curs.class, nom);
+        return curs;
     }
 
     public Matricula apunta(String alumne, String curs) throws EdatIncorrecteException {
