18aae5467b27c5beb50e5bd8718766d96a515cba
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-18 18:41:54 +0200

    test_mapping_curs passed :D

--- 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Curs.java
+++ 10_setmana_2_exercici/src/main/java/org/formacio/setmana2/domini/Curs.java
@@ -1,23 +1,39 @@
 package org.formacio.setmana2.domini;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "t_cursos")
 public class Curs {
 
-	private String nom;
-    
-	private int edatMinima;
-	
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public int getEdatMinima() {
-		return edatMinima;
-	}
-	public void setEdatMinima(int edatMinima) {
-		this.edatMinima = edatMinima;
-	}
-	
-	
+    /* ---- Properties ---- */
+    @Id
+    @Column(name = "cur_nom")
+    private String nom = null;
+    @Column(name = "cur_edatminima")
+    private int edatMinima = 0;
+
+
+    /* ---- Getters ---- */
+    public String getNom() {
+        return nom;
+    }
+
+    public int getEdatMinima() {
+        return edatMinima;
+    }
+
+
+    /* ---- Setters ---- */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setEdatMinima(int edatMinima) {
+        this.edatMinima = edatMinima;
+    }
 }
